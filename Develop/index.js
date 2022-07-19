154eb188d56ed8863ad09a17033874774349d9e2
Avyrie Fellows <avyriefellows@gmail.com>
2022-07-19 10:38:21 -0600

    Made licenses into object.keys, created functions writeToFile and init

--- Develop/index.js
+++ Develop/index.js
@@ -1,63 +1,92 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const {licenses, generateMarkdown} = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type: "input",
-        message: "What is your project title?",
-        name: "title",
-        default: "Project Title",
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+        default: 'Project Title',
     },
     {
-        type: "input",
-        message: "What is the description of your project?",
-        name: "description",
+        type: 'input',
+        message: 'What is the description of your project?',
+        name: 'description',
     },
     {
-        type: "input",
-        message: "What are the installation instructions?",
-        name: "installation",
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'installation',
     },
     {
-        type: "input",
-        message: "What is the usage information?",
-        name: "usage",
+        type: 'input',
+        message: 'What is the usage information?',
+        name: 'usage',
     },
     {
-        type: "input",
-        message: "What are the contribution guidelines?",
-        name: "contribution",
+        type: 'input',
+        message: 'What are the contribution guidelines?',
+        name: 'contribution',
     },
     {
-        type: "input",
-        message: "What are the test instructions?",
-        name: "test",
+        type: 'input',
+        message: 'What are the test instructions?',
+        name: 'test',
     },
     {
-        type: "list",
-        message: "What license do you wish to use?",
-        name: "license",
-        choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause 'Simplified' License", "BSD 3-Clause 'New' or 'Revised' License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1", "Mozilla Public License 2.0", "The Unlicense", "No license"],
+        type: 'list',
+        message: 'What license do you wish to use?',
+        name: 'license',
+        choices: Object.keys(licenses),
     },
     {
-        type: "input",
-        message: "What is your GitHub username?",
-        name: "username",
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
     },
     {
-        type: "input",
-        message: "What is your email address?",
-        name: "email",
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.stat('build', (err, stat) => {
+        if (err) {
+            fs.mkdirSync('build');
+        }
+        fs.writeFile('build/' + fileName, data, err => {
+            if (err) {
+                return console.log(err);
+            }
+            console.log('Congratulations! Your README.md file has been created!!');
+        });
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+
+        //Inquirer questions
+        const responses = await inquirer.prompt(questions);
+        console.log('Submitted repsonses: ', responses);
+
+        //Inserts responses into generateMarkdown
+        const markdown = generateMarkdown(responses);
+
+        //Makes markdown into file? Uses writeToFile function.
+        writeToFile('README.md', markdown);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
 
 // Function call to initialize app
 init();
